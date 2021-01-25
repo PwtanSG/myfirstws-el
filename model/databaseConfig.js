1fd908cd3619c0feae29fe0f8d479a91fc1c5545
pwtan <pwtan.edu@gmail.com>
2021-01-25 21:15:36 +0800

    add aws-rds-endpt

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -5,9 +5,10 @@ var dbConnect={
     getConnection:function(){
         var conn=mysql.createConnection({
            // host:"furniture.c2cj27untbbn.us-east-1.rds.amazonaws.com",
-           host:"rds-mysql-10mintutorial.ckpwduroihfq.us-east-1.rds.amazonaws.com",
-            user:"root",
-            password:"root1234",
+           //host:"rds-mysql-10mintutorial.ckpwduroihfq.us-east-1.rds.amazonaws.com",
+	    host:"bdd-db.csn6grmspbkl.us-east-1.rds.amazonaws.com",
+            user:"admin88",
+            password:"12345678",
             database:"furniture"
 
         }
